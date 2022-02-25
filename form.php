50cc2f46c34e1a318ed79c11c8e153f67a0b699e
MadelM10 <40964967+MadelM10@users.noreply.github.com>
2022-02-25 20:45:03 +0200

    add main files

---
+++ form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Web Contact Form</title>
+</head>
+<body>
+	<form action="formData.php" method="post">
+		<input type="text" name="username">
+		<input type="email" name="email">
+		<select>
+			<option>Query</option>
+			<option>Feedback</option>
+			<option>Complaint</option>
+			<option>Other</option>
+		</select>
+		<textarea></textarea>
+	</form>
+</body>
+</html>
